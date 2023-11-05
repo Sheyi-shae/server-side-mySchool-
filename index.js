cb27ba16192095a9077f1310d135bce0e55e5add
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2023-11-05 12:00:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ require('dotenv').config();// environment
 
 
 // Middleware
-app.use(cors({
-  origin: 'https://my-school-full-stack-client.vercel.app',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/topicuploads', express.static('topicuploads'));
 app.use('/uploads', express.static('uploads'));
